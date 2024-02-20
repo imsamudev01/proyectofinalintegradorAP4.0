2bdc7d4fafb7e6acef8d0d96da19b1ffa51c2fc4
SamuDev <samudev01@gmail.com>
2024-02-20 20:47:24 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+
+let nombre = prompt ("Hey, ¿Cómo te llamas?")
+
+alert (`Hola ${nombre} siéntete a gusto examinando mi portfolio ficticio, proyecto final integrador de AP4.0 😉`)
+
+// aquí finalizo con Javascript, tengo conocimiento de los otros tipos de datos pero no sabría aplicarlos en un portfolio por el momento. 
